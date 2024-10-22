f521097aa462a0dedfff2f7d5defac27bc5c09cf
AkmalIrsyad <akmalirsyad137@gmail.com>
2024-10-22 21:36:56 +0700

    membuat halaman login dan logout php, dan konek ke mysqsl

--- adminpanel/index.php
+++ adminpanel/index.php
@@ -1,5 +1,5 @@
 <?php
-
+    require "session.php";
 ?>
 
 <!DOCTYPE html>
@@ -7,9 +7,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>test</title>
+    <title>Document</title>
 </head>
 <body>
-    
+    <h2>Hallo <?php echo $_SESSION['username']; ?></h2>
 </body>
 </html>
